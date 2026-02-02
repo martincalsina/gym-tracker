c08afe2256a2f99f63d17119fc757379c60aee05
martincalsina <martincalsina05@gmail.com>
2026-02-02 19:58:22 -0300

    adds icons for tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,36 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
     return (
         <Tabs>
-            <Tabs.Screen name="home" options={{title: "Home", tabBarLabel: 'Home'}}/>
-            <Tabs.Screen name="workouts" options={{title: "Workouts", tabBarLabel: 'Workouts'}}/>
-            <Tabs.Screen name="progress" options={{title: "Progress", tabBarLabel: 'Progress'}}/>
+            <Tabs.Screen 
+                name="home" 
+                options={{
+                    title: "Home", 
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+                    ),
+                }}/>
+            <Tabs.Screen 
+                name="workouts" 
+                options={{
+                    title: "Workouts", 
+                    tabBarLabel: 'Workouts',
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons name={focused ? 'barbell-sharp' : 'barbell-outline'} color={color} size={24} />
+                    ),
+                }}/>
+            <Tabs.Screen 
+                name="progress" 
+                options={{
+                    title: "Progress", 
+                    tabBarLabel: 'Progress',
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons name={focused ? 'analytics-sharp' : 'analytics-outline'} color={color} size={24}/>
+                    ) 
+                }}/>
         </Tabs>
     )
 }
